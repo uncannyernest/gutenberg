e6b46c99dc9c8433dd22044ff748073dccf2d0a8
Jorge <jorge.costa@developer.pt>
2018-04-23 16:22:35 +0100

    Improved RangeControl condition.
     Checked for undefined in initialSliderValue did not catch the case where the value is equal to empty string for example. The condition was improved to handle this case.

--- components/range-control/index.js
+++ components/range-control/index.js
@@ -26,7 +26,7 @@ function RangeControl( {
 } ) {
 	const id = `inspector-range-control-${ instanceId }`;
 	const onChangeValue = ( event ) => onChange( Number( event.target.value ) );
-	const initialSliderValue = value === undefined ? initialPosition || '' : value;
+	const initialSliderValue = ! value && 0 !== value ? initialPosition || '' : value;
 
 	return (
 		<BaseControl
